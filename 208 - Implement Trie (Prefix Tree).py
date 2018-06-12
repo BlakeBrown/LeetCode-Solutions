cd532a1370137ea0def2a1eb7da291993aea61b2
Blake Brown <blakebrown129@gmail.com>
2018-06-12 12:11:20 -0400

    Solve #208

---
+++ 208 - Implement Trie (Prefix Tree).py
@@ -0,0 +1,63 @@
+class Node(object):
+    def __init__(self,count):
+        # self.value = value
+        self.count = count
+        self.children = {}
+
+class Trie(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node(0)
+        
+        
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Node(0)
+            node = node.children[c]
+        node.count += 1
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        if node.count == 0:
+            return False
+        return True
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
