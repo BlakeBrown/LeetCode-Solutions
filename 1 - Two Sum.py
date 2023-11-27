e42982dde1d97b5d7bc9ac4d1ac4ca3047a943c7
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-27 18:20:57 +0530

    hurahhhh!

--- 1 - Two Sum.py
+++ 1 - Two Sum.py
@@ -15,3 +15,12 @@ class Solution:
                     return [numsDict[target-i][0],numsDict[i][0]]
                 elif len(numsDict[target-i]) > 1:
                     return numsDict[target-i]
+
+# simple & elegant
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+
