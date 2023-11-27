5f189f7869098a0ee80297be143f980b691adfdf
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-27 18:25:23 +0530

    huraah again! | 11/27

--- 90 - Subsets II.py
+++ 90 - Subsets II.py
@@ -59,3 +59,14 @@ class Solution(object):
             i += dupes
         return pset
 
+# Solution 3 - MB-44
+class Solution(object):
+    def subsetsWithDup(self,nums):
+        subsets = [[]]
+        if len(nums) == 1:
+            subsets.append(nums[0])
+        else: 
+            for i in range(len(nums)-1):
+                for j in range(i,len(nums)):
+                    subsets.append(nums[i:j+1])
+        return subsets
